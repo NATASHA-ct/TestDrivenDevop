7f551ab84e6b2817daad42b175468380b07b702b
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-13 14:32:12 +0300

    Add test for factorial of negatives

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,16 +1,30 @@
 require_relative '../solver'
 
 describe Solver do
+
+#test for factorial method    
  it 'factorial returns the multiplication of all positive integers' do
     solver = Solver.new(5)
-
     expect(solver.factorial).to eq(120)
   end
 
   it 'factorial of zero return 1' do
     solver = Solver.new(0)
-
     expect(solver.factorial).to eq(1)
+  end
+
+  it 'factorial of negative number raises an error' do
+    solver = Solver.new(-1)
+    expect { solver.factorial }.to raise_error('Factorial of negative number is not defined')
+  end
+
+
+
+
+#test for fizzbuzz method
+  it 'FizzBuzz returns FizzBuzz when its a multiple of both 3 and 5' do
+    solver = Solver.new(15)
+    expect(solver.fizzbuzz).to eq('FizzBuzz')
   end
 
 end
