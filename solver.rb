e7102a4beef9839595083c842d6b8c84465515f9
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-13 13:52:40 +0300

    Modify factorial method

--- solver.rb
+++ solver.rb
@@ -6,7 +6,16 @@ class Solver
    end
 
    def factorial
-   
+     if @num.zero?
+      1
+    elsif @num.negative?
+      raise 'Factorial of negative number is not defined'
+    else
+      digits = 1..@num
+      result = 1
+      digits.each { |num| result *= num }
+      result
+    end
    end
 
 end
